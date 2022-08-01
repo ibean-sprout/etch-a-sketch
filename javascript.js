b8ce3ea31c539d2172132225ce931670e9c3d096
sgreenebean <sgreene929@protonmail.com>
2022-08-01 10:31:09 -0500

    set up boiler plate and js divs

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//create 16X16 squares of divs
+const container = document.createElement('div');
+const sketchDivs = document.createElement('div');
+sketchDivs.classList.add('content');
+sketchDivs.style.cssText = 'width: 300px; height: 300px; color: lightblue';
+
+container.appendChild(sketchDivs);
+
+
