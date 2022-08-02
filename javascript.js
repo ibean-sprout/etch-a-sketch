2a3da176c8729837ddb4ecb78ad2bf8d227b6808
sgreenebean <sgreene929@protonmail.com>
2022-08-02 11:00:03 -0500

    finialize the grid using javascript and css. centered to the screen by appying 100vh to both height and width of container and using margin auto.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,31 @@
 //create 16X16 squares of divs
-const container = document.createElement('div');
-const sketchDivs = document.createElement('div');
-sketchDivs.classList.add('content');
-sketchDivs.style.cssText = 'width: 300px; height: 300px; color: lightblue';
+// function createDivs(rows, columns) {
 
-container.appendChild(sketchDivs);
+//     let container = document.querySelector('#container');
+        
+//     container.style.setProperty('--grid-rows', rows);
+//     container.style.setProperty('--grid-columns', columns);
+//     for (let i = 0; i < (rows * columns); i++) {
+//         let sketchDivs = document.createElement('div');
+//         sketchDivs.classList.add('content');
+//         sketchDivs.style.cssText = 'width: 30px; height: 30px; color: lightblue';
+//         sketchDivs.style.border = 'solid black .5px';
+//         container.appendChild(sketchDivs).className = 'grid-item';
+//     } 
+// }
+function createDivs() {
+    let container = document.querySelector('#container');
+    for ( let i = 0; i < 256; i++) {
+        let sketchDivs = document.createElement('div');
+        sketchDivs.classList.add('content');
+        sketchDivs.style.cssText = 'width: 50px; height: 50px; color: lightblue';
+        sketchDivs.style.border = 'solid black .5px';
+        container.appendChild(sketchDivs);
+
+    }
 
 
+}
+
+createDivs();
+//let sixteenDivs = createDivs * 16;
