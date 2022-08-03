04309c8c5929fa30588d06b778a634fc81ee4355
sgreenebean <sgreene929@protonmail.com>
2022-08-03 12:27:19 -0500

    create pixel effect on divs

--- javascript.js
+++ javascript.js
@@ -1,31 +1,57 @@
 //create 16X16 squares of divs
-// function createDivs(rows, columns) {
+let container = document.querySelector('#container');
+let sketchDivs; 
 
-//     let container = document.querySelector('#container');
-        
-//     container.style.setProperty('--grid-rows', rows);
-//     container.style.setProperty('--grid-columns', columns);
-//     for (let i = 0; i < (rows * columns); i++) {
-//         let sketchDivs = document.createElement('div');
-//         sketchDivs.classList.add('content');
-//         sketchDivs.style.cssText = 'width: 30px; height: 30px; color: lightblue';
-//         sketchDivs.style.border = 'solid black .5px';
-//         container.appendChild(sketchDivs).className = 'grid-item';
-//     } 
-// }
 function createDivs() {
-    let container = document.querySelector('#container');
+    
     for ( let i = 0; i < 256; i++) {
-        let sketchDivs = document.createElement('div');
+        sketchDivs = document.createElement('div');
         sketchDivs.classList.add('content');
         sketchDivs.style.cssText = 'width: 50px; height: 50px; color: lightblue';
         sketchDivs.style.border = 'solid black .5px';
         container.appendChild(sketchDivs);
 
+        //creating a function with the parameter set made the hover effect stick
+        changeDivColor(sketchDivs);
     }
+    
 
 
 }
 
 createDivs();
+
+//create hover/pixel effect
+function changeDivColor(div) {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'lightblue';
+    })
+}
+
+// let changeDivColors = document.getElementsByClassName('content');
+// // console.log(changeDivColors);
+
+// for (let i = 0; i < changeDivColors.length; i++) {
+//     changeDivColors[i].addEventListener('mouseover', () => {
+//         sketchDivs.style.backgroundColor = 'lightblue';
+// });
+// }
+//         // color.addEventListener('mouseleave', () => {
+//         //     sketchDivs.style.backgroundColor = 'red';
+//         // })
+// })
+
+
+// function createPixels() {
+//         let pixels = sketchDivs.addEventListener('mouseover', () => {
+ //   sketchDivs.style.backgroundColor = 'lightblue';
+//     })
+//     pixels.addEventListener('mouseleave', () => {
+//         sketchDivs.style.backgroundColor = 'lightblue';
+//     })
+// }
+
+
 //let sixteenDivs = createDivs * 16;
+
+//create pixelated effect with hover
